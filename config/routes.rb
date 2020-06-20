101d1338ee44672b8b5c11e56eaad8f821cbdcc5
Your <scarlet3628@likelion.org>
2020-06-20 14:14:47 +0900

    before adding blog posting

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   resources :order_items
   get 'cart', to: 'cart#show'
   resources :blogs
+
+
   devise_for :users
   root 'posts#index'
   get '/posts/guide', to:'posts#guide'
   get '/posts/shop', to:'posts#shop'
   get '/posts/hashtag/:name', to: 'posts#hashtags'
+
 
   resources :guides
   resources :shops
