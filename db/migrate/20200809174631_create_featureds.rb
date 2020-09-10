2fe5ce729720c0a2709191bcb37863a991e9273e
Your <scarlet3628@likelion.org>
2020-09-11 00:20:33 +0900

    before adding new columns

--- db/migrate/20200809174631_create_featureds.rb
+++ db/migrate/20200809174631_create_featureds.rb
@@ -5,35 +5,6 @@ class CreateFeatureds < ActiveRecord::Migration[5.2]
       t.string :furl
       t.string :fimage
 
-      t.string :f_image_01
-      t.string :f_image_02
-      t.string :f_image_03
-      t.string :f_image_04
-      t.string :f_image_05
-      t.string :f_image_06
-
-      t.string :f_subtitle_01
-      t.string :f_subtitle_02
-      t.string :f_subtitle_03
-      t.string :f_subtitle_04
-      t.string :f_subtitle_05
-      t.string :f_subtitle_06
-
-      t.string :f_link_01
-      t.string :f_link_02
-      t.string :f_link_03
-      t.string :f_link_04
-      t.string :f_link_05
-      t.string :f_link_06
-
-      t.text :f_description_01
-      t.text :f_description_02
-      t.text :f_description_03
-      t.text :f_description_04
-      t.text :f_description_05
-      t.text :f_description_06
-
-
       t.timestamps
     end
   end
