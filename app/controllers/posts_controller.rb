dc0b9f2035c56b309f08e660cb4b20cc0b51dab9
Your <scarlet3628@likelion.org>
2020-09-16 19:29:35 +0900

    more seller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -109,7 +109,8 @@ class PostsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :link, :category_id, :shop_id, :guide_id, :post_image, :source, :video, :image_toggle,
-                                   :description, :is_price, :was_price, :pct, :p_spare_01, :p_spare_02, :p_spare_03, :p_spare_04, :p_spare_05)
+                                   :description, :is_price, :was_price, :pct, :p_spare_01, :p_spare_02, :p_spare_03, :p_spare_04, 
+                                   :p_spare_05, :p_spare_06)
     end
 end
 
