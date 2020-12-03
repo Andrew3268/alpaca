1b09dfa3b4263e4924c59f2768dc7e3917951f65
Your <scarlet3628@likelion.org>
2020-12-04 02:30:01 +0900

    shops

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :shop]
   before_action :authenticate_user!, except: [:index, :show, :shop, :hashtags]
   # skip_authorization_check only: :hashtags
 
